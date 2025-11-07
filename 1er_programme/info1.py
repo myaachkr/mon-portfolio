5f6f66f532be1277e20cbeec5178a88d782d5fb3
myaachkr <myaelachkar@gmail.com>
2025-11-07 11:06:34 +0100

    Delete 1er_programme/info1.py

--- 1er_programme/info1.py
+++
@@ -1,6 +0,0 @@
-def droite(x,y) :
-    return 2*+x+3*y-2
-a = float(input("saisir un réel"))
-b = float(input("saisir un réel"))
-print ("droite (a,b)= ", droite (a,b)  )
-print ("droite (1,0)= ", droite (1,0)  )
